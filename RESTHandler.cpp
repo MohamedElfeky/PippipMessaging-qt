27883f99d5587a8af9b77d20bf299f4a14e24758
Steve Brenneis <steve.brenneis@secomm.org>
2017-03-07 14:42:31 -0500

    Periodic commit

--- RESTHandler.cpp
+++ RESTHandler.cpp
@@ -1,6 +1,51 @@
-#include "RESThandler.h"
+/*
+ * RESTHandler.cpp
+ * Copyright (C) 2017 Steve Brenneis <steve.brenneis@secomm.org>
+ *
+ * PippipMessaging is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PippipMessaging is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-RESTHandler::RESTHandler(QObject *parent) :
-    QObject(parent)
-{
+#include "RESTHandler.h"
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+RESTHandler::RESTHandler(QObject *parent)
+: QObject(parent),
+  success(false) {
+
+    manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(managerFinished(QNetworkReply*)));
+
+}
+
+RESTHandler::~RESTHandler() {
+
+    delete manager;
+
+}
+
+void RESTHandler::managerFinished(QNetworkReply *reply) {
+
+    QNetworkReply::NetworkError errorType = reply->error();
+    if (errorType == QNetworkReply::NoError) {
+        response = reply->readAll();
+        success = true;
+    }
+    else {
+        error = reply->errorString();
+    }
+
+    emit requestComplete(this);
+
 }
--- RESTHandler.h
+++ RESTHandler.h
@@ -1,17 +1,55 @@
+/*
+ * RESTHandler.h
+ * Copyright (C) 2017 Steve Brenneis <steve.brenneis@secomm.org>
+ *
+ * PippipMessaging is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PippipMessaging is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef RESTHANDLER_H
 #define RESTHANDLER_H
 
 #include <QObject>
+#include <QByteArray>
+#include <QString>
 
-class RESTHandler : public QObject
-{
+class QNetworkAccessManager;
+class QNetworkReply;
+
+class RESTHandler : public QObject {
     Q_OBJECT
-public:
-    explicit RESTHandler(QObject *parent = 0);
 
-signals:
+    public:
+        explicit RESTHandler(QObject *parent = 0);
+        ~RESTHandler();
 
-public slots:
+    public:
+        const QString& getError() const { return error; }
+        const QByteArray&  getResponse() const { return response; }
+        bool hasError() const { return !success; }
+
+    signals:
+        void requestComplete(RESTHandler *handler);
+
+    private slots:
+        void managerFinished(QNetworkReply* reply);
+
+    private:
+        QNetworkAccessManager *manager;
+
+        QByteArray response;
+        QString error;
+        bool success;
 
 };
 
