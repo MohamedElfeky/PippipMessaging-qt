fd94bfae2a77b101997bf3cdcc2a6f04a361bf7d
Steve Brenneis <steve.brenneis@secomm.org>
2017-07-17 17:50:34 -0400

    Initial commit.

---
+++ StringCodec.cpp
@@ -0,0 +1,57 @@
+/*
+ * StringCodec.cpp
+ * Copyright (C) 2017 Steve Brenneis <steve.brenneis@secomm.org>
+ *
+ * PippipMessaging is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PippipMessaging is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "StringCodec.h"
+#include <QByteArray>
+#include <string.h>
+
+namespace Pippip {
+
+StringCodec::StringCodec(const QString& q)
+: qtString(q),
+  qt(true),
+  strLength(q.length()) {
+
+    QByteArray data = qtString.toUtf8();
+    cstr = new char[strLength + 1];
+    memset(cstr, 0, strLength + 1);
+    strncpy(cstr, data.constData(), strLength);
+    stdString = std::string(cstr);
+
+}
+
+StringCodec::StringCodec(const std::string& c)
+: stdString(c),
+  qt(false),
+  strLength(c.length()) {
+
+    cstr = new char[strLength + 1];
+    strcpy(cstr, stdString.c_str());
+    qtString = QString(cstr);
+
+}
+
+StringCodec::~StringCodec() {
+
+    if (!qt) {
+        delete[] cstr;
+    }
+
+}
+
+}
---
+++ StringCodec.h
@@ -0,0 +1,56 @@
+/*
+ * StringCodec.h
+ * Copyright (C) 2017 Steve Brenneis <steve.brenneis@secomm.org>
+ *
+ * PippipMessaging is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PippipMessaging is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STRINGCODEC_H
+#define STRINGCODEC_H
+
+#include <QString>
+#include <string>
+
+namespace Pippip {
+
+class StringCodec {
+
+    public:
+        StringCodec(const QString& qtString);
+        StringCodec(const std::string& stdString);
+        StringCodec(const char *str);   // Null terminated string
+        ~StringCodec();
+
+    public:
+        operator const char* () const { return cstr; }
+        operator const QString& () const { return qtString; }
+        operator const std::string& () const { return stdString; }
+
+    public:
+        const std::string& getStdString() const { return stdString; }
+        const QString& getQtString() const { return qtString; }
+        size_t length() const { return strLength; }
+
+    private:
+        QString qtString;
+        std::string stdString;
+        bool qt;
+        char* cstr;
+        size_t strLength;
+
+};
+
+}
+
+#endif // STRINGCODEC_H
